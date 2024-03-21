966d4ce835d1ce1bfa4d3006804524ba680f0875
arthuradriaens-code <arthur99.adriaens@gmail.com>
2024-03-21 15:02:33 +0100

    Made a lot of progress :)

--- MGO.jl
+++ MGO.jl
@@ -1,15 +1,19 @@
 using ForwardDiff
 using OrdinaryDiffEq
 
-function TraceRay(xk0::AbstractVector,ω0,τmin,τmax,D,rtol=1e-3,τsteps=1000)
+function TraceRayIP(xk0,ω0,τmin,τmax,D,τsteps=1000)
     # xk = [x,y,z,kx,ky,kz]
-    function RHS(xk::AbstractVector,p,t)
+    function RHS!(dxk,xk,p,t) #Ray Hamilton's equationS
         x_ = xk[1:3]
         k_ = xk[4:6]
         JD(xk) = ForwardDiff.jacobian(xk -> D(xk[1:3],xk[4:6]),xk)
-        [JD(xk)[1,1],JD(xk)[2,2],JD(xk)[3,3],JD(xk)[1,4],JD(xk)[2,5],JD(xk)[3,6]]
+        dxk[1] = JD(xk)[1,4]
+        dxk[2] = JD(xk)[2,5]
+        dxk[3] = JD(xk)[3,6]
+        dxk[4] = -JD(xk)[1,1]
+        dxk[5] = -JD(xk)[2,2]
+        dxk[6] = -JD(xk)[3,3]
     end
-
-    prob = ODEProblem(RHS,xk0,(τmin,τmax))
-    sol = solve(prob,Tsit5(),reltol=rtol)
+    prob = ODEProblem(RHS!,xk0,(τmin,τmax))
+    sol = solve(prob,Tsit5())
 end
--- TryOut.jl
+++ TryOut.jl
@@ -1,7 +1,7 @@
 include("MGO.jl")
 using ForwardDiff
 using OrdinaryDiffEq
-using PyPlot
+using Plots
 
 function D(x::AbstractVector,k::AbstractVector)
     -k.*k - x
@@ -11,5 +11,5 @@ end
 τsteps = 1000
 τmax = 1
 τ = range(τmin,τmax,1000)
-sol = TraceRay([1.0,1.0,0,0,0,1.1],0,τmin,τmax,D::Function)
-plot(τ,sol.u[1,:])
+sol = TraceRayIP([1.0,1.0,0,0,0,1.1],0,τmin,τmax,D::Function)
+plot(sol,show=true)
