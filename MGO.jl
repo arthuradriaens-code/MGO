062dcac9e1afec58a47d74ff62390202619e29f3
arthuradriaens-code <arthur99.adriaens@gmail.com>
2024-03-25 17:04:33 +0100

    I am miserable

--- MGO.jl
+++ MGO.jl
@@ -30,3 +30,27 @@ function TraceRayIP(xk0,ω0,τmin,τmax,D,τsteps=1000)
 
     return t,x,y,z,kx,ky,kz
 end
+function TraceRayIP2D(xk0,ω0,τmin,τmax,D,τsteps=1000)
+    # xk = [x,y,kx,ky]
+    function RHS!(dxk,xk,p,t) #Ray Hamilton's equations
+        x_ = xk[1:2]
+        k_ = xk[3:4]
+        JD(xk) = ForwardDiff.jacobian(xk -> D(x_,k_),xk)
+        dxk[1] = JD(xk)[1,4]
+        dxk[2] = JD(xk)[2,4]
+        dxk[3] = -JD(xk)[1,1]
+        dxk[4] = -JD(xk)[2,2]
+    end
+    probf = ODEProblem(RHS!,xk0,(τmin,τmax)) #forward in time
+    solf = solve(probf,Tsit5(),saveat=0.1)
+    probb = ODEProblem(RHS!,xk0,(τmin,τmin-0.2)) #back in time
+    solb = solve(probb,Tsit5(),saveat=0.1)
+
+    #joining backwards and forwards, defining t,x,y,z,kx,ky,kz
+    x = [reverse(reduce(vcat,transpose.(solb.u))[:,1]);reduce(vcat,transpose.(solf.u))[:,1]]
+    y = [reverse(reduce(vcat,transpose.(solb.u))[:,2]);reduce(vcat,transpose.(solf.u))[:,2]]
+    kx = [reverse(reduce(vcat,transpose.(solb.u))[:,3]);reduce(vcat,transpose.(solf.u))[:,3]]
+    ky = [reverse(reduce(vcat,transpose.(solb.u))[:,4]);reduce(vcat,transpose.(solf.u))[:,4]]
+    t = [reverse(reduce(vcat,transpose.(solb.t)));reduce(vcat,transpose.(solf.t))]
+    return t,x,y,kx,ky
+end
--- TryOut.jl
+++ TryOut.jl
@@ -8,10 +8,29 @@ function D(x::AbstractVector,k::AbstractVector)
     -k.*k - x
 end
 
+function ComputeRayManifold(τmin,τmax,kx0,ky0,D::Function,y0s::AbstractVector)
+    xk0 = [0.0,y0s[1],0,kx0,ky0,0.1]
+    t,x,y,z,kx,ky,kz = TraceRayIP(xk0,0,τmin,τmax,D::Function)
+    Ray = [t,x,y,z,kx,ky,kz]
+    RayManifold = [Ray]
+    for y0 in y0s[2:size(y0s)[1]-1]
+        xk0 = [0.0,y0,0,kx0,ky0,0.1]
+        t,x,y,z,kx,ky,kz = TraceRayIP(xk0,0,τmin,τmax,D::Function)
+        Ray = [t,x,y,z,kx,ky,kz]
+        RayManifold = [RayManifold;[Ray[:]]]
+    end
+    RayManifold
+end
+
 τmin = 0
 τsteps = 1000
 τmax = 1
 τ = range(τmin,τmax,1000)
-xk0 = [1.0,1.0,0,0,0,1.1]
-t,x,y,z,kx,ky,kz = TraceRayIP(xk0,0,τmin,τmax,D::Function)
-plot(x,y,z,show=true)
+#xk0 = [1.0,1.0,0,0,0,1.1]
+#t,x,y,z,kx,ky,kz = TraceRayIP(xk0,0,τmin,τmax,D::Function)
+y0s = LinRange(-1,1,20)
+RayManifold = ComputeRayManifold(τmin,τmax,0.01,0.7,D::Function,y0s)
+plot()
+for Ray in RayManifold
+    plot!(Ray[2],Ray[3],show=true)
+end
