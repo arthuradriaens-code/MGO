9244532ec86ec2f68239a8446c2fcc8b78521fc9
arthuradriaens-code <arthur99.adriaens@gmail.com>
2024-03-25 09:52:18 +0100

    added solution

--- TryOut.jl
+++ TryOut.jl
@@ -3,6 +3,7 @@ using ForwardDiff
 using OrdinaryDiffEq
 using Plots
 
+#Depends on system
 function D(x::AbstractVector,k::AbstractVector)
     -k.*k - x
 end
@@ -11,5 +12,7 @@ end
 τsteps = 1000
 τmax = 1
 τ = range(τmin,τmax,1000)
-sol = TraceRayIP([1.0,1.0,0,0,0,1.1],0,τmin,τmax,D::Function)
-plot(sol,show=true)
+xk0 = [1.0,1.0,0,0,0,1.1]
+solf = TraceRayIP(xk0,0,τmin,τmax,D::Function)
+solb = TraceRayIP(xk0,0,τmin,τmin-0.2,D::Function)
+plot(solf,show=true)
