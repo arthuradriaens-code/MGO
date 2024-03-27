54ef954eb562e319603486c2087b2659439ae7bc
arthuradriaens-code <arthur99.adriaens@gmail.com>
2024-03-27 18:07:43 +0100

    almost implemented airy :)

--- 1D/MGO.jl
+++ 1D/MGO.jl
@@ -1,56 +1,47 @@
 using ForwardDiff
 using OrdinaryDiffEq
+using Interpolations
 
-function TraceRayIP(xk0,ω0,τmin,τmax,D,τsteps=1000)
-    # xk = [x,y,z,kx,ky,kz]
+function TraceRay(xk0,τmin,τmax,D,boundaryx,τsteps=1000)
+    # terminate integration if boundary is reached
+    condition(xk, t, integrator) = xk[1] - boundaryx
+    affect!(integrator) = terminate!(integrator)
+    cb = ContinuousCallback(condition, affect!)
+
     function RHS!(dxk,xk,p,t) #Ray Hamilton's equations
-        x_ = xk[1:3]
-        k_ = xk[4:6]
-        JD(xk) = ForwardDiff.jacobian(xk -> D(xk[1:3],xk[4:6]),xk)
-        dxk[1] = JD(xk)[1,4]
-        dxk[2] = JD(xk)[2,5]
-        dxk[3] = JD(xk)[3,6]
-        dxk[4] = -JD(xk)[1,1]
-        dxk[5] = -JD(xk)[2,2]
-        dxk[6] = -JD(xk)[3,3]
+        JD(xk) = ForwardDiff.gradient(xk -> D(xk),xk)
+        ∂xD = JD(xk)[1]
+        ∂kD = JD(xk)[2]
+        dxk[1] = -∂kD
+        dxk[2] = ∂xD
     end
-    probf = ODEProblem(RHS!,xk0,(τmin,τmax)) #forward in time
-    solf = solve(probf,Tsit5(),saveat=0.1)
-    probb = ODEProblem(RHS!,xk0,(τmin,τmin-0.2)) #back in time
-    solb = solve(probb,Tsit5(),saveat=0.1)
+    prob = ODEProblem(RHS!,xk0,(τmin,τmax)) #forward in time
+    sol = solve(prob,Tsit5(),saveat=0.1,callback=cb)
+    #probb = ODEProblem(RHS!,xk0,(τmin,τmin-0.2)) #back in time
+    #solb = solve(probb,Tsit5(),saveat=0.1)
 
     #joining backwards and forwards, defining t,x,y,z,kx,ky,kz
-    x = [reverse(reduce(vcat,transpose.(solb.u))[:,1]);reduce(vcat,transpose.(solf.u))[:,1]]
-    y = [reverse(reduce(vcat,transpose.(solb.u))[:,2]);reduce(vcat,transpose.(solf.u))[:,2]]
-    z = [reverse(reduce(vcat,transpose.(solb.u))[:,3]);reduce(vcat,transpose.(solf.u))[:,3]]
-    kx = [reverse(reduce(vcat,transpose.(solb.u))[:,4]);reduce(vcat,transpose.(solf.u))[:,4]]
-    ky = [reverse(reduce(vcat,transpose.(solb.u))[:,5]);reduce(vcat,transpose.(solf.u))[:,5]]
-    kz = [reverse(reduce(vcat,transpose.(solb.u))[:,6]);reduce(vcat,transpose.(solf.u))[:,6]]
-    t = [reverse(reduce(vcat,transpose.(solb.t)));reduce(vcat,transpose.(solf.t))]
-
-    return t,x,y,z,kx,ky,kz
+    #x = [reverse(reduce(vcat,transpose.(solb.u))[:,1]);reduce(vcat,transpose.(solf.u))[:,1]]
+    #kx = [reverse(reduce(vcat,transpose.(solb.u))[:,4]);reduce(vcat,transpose.(solf.u))[:,4]]
+    #t = [reverse(reduce(vcat,transpose.(solb.t)));reduce(vcat,transpose.(solf.t))]
+    extract_sol(sol)
 end
-function TraceRayIP2D(xk0,ω0,τmin,τmax,D,τsteps=1000)
-    # xk = [x,y,kx,ky]
-    function RHS!(dxk,xk,p,t) #Ray Hamilton's equations
-        x_ = xk[1:2]
-        k_ = xk[3:4]
-        JD(xk) = ForwardDiff.jacobian(xk -> D(x_,k_),xk)
-        dxk[1] = JD(xk)[1,4]
-        dxk[2] = JD(xk)[2,4]
-        dxk[3] = -JD(xk)[1,1]
-        dxk[4] = -JD(xk)[2,2]
-    end
-    probf = ODEProblem(RHS!,xk0,(τmin,τmax)) #forward in time
-    solf = solve(probf,Tsit5(),saveat=0.1)
-    probb = ODEProblem(RHS!,xk0,(τmin,τmin-0.2)) #back in time
-    solb = solve(probb,Tsit5(),saveat=0.1)
-
-    #joining backwards and forwards, defining t,x,y,z,kx,ky,kz
-    x = [reverse(reduce(vcat,transpose.(solb.u))[:,1]);reduce(vcat,transpose.(solf.u))[:,1]]
-    y = [reverse(reduce(vcat,transpose.(solb.u))[:,2]);reduce(vcat,transpose.(solf.u))[:,2]]
-    kx = [reverse(reduce(vcat,transpose.(solb.u))[:,3]);reduce(vcat,transpose.(solf.u))[:,3]]
-    ky = [reverse(reduce(vcat,transpose.(solb.u))[:,4]);reduce(vcat,transpose.(solf.u))[:,4]]
-    t = [reverse(reduce(vcat,transpose.(solb.t)));reduce(vcat,transpose.(solf.t))]
-    return t,x,y,kx,ky
+function extract_sol(sol)
+    xs = first.(sol.u);
+    ks = last.(sol.u);
+    #create matrix
+    zs = hcat(xs, ks);
+    t = sol.t;
+    nt = size(t);
+    return sol,t,nt,xs,ks,zs
+end
+function get_go_field(sol,phi0)
+    sol,t,nt,xs,ks,zs = extract_sol(sol);
+    #https://discourse.julialang.org/t/
+    #differentiation-without-explicit-function-np-gradient/57784/2
+    itpx = interpolate((t,), xs, Gridded(Linear()));
+    itpk = interpolate((t,), ks, Gridded(Linear()));
+    ∂τx = Interpolations.gradient.(Ref(itpx),t);
+    ∂τk = Interpolations.gradient.(Ref(itpk),t);
+    return itpx,itpk
 end
--- 1D/TryOut.jl
+++ 1D/TryOut.jl
@@ -5,13 +5,20 @@ using SpecialFunctions
 using Plots
 
 #Depends on system
-function D(x,k)
+function D(xk::AbstractVector)
+    x = xk[1]
+    k = xk[2]
     -x - k*k
 end
 
-τ1min = -8
+τ1min = 0
 τ1steps = 1000
-τ1max = 0
+τ1max = 8
 τ1 = range(τ1min,τ1max,τ1steps)
-plot(τ1,airyai.(τ1),show=true) #reference exact solution
-ϕ0 = Ai(τ1min)
+#plot(τ1,airyai.(τ1),show=true) #reference exact solution
+xk0 = [-8,sqrt(8)]
+phi0 = airyai(-8)
+sol,t,nt,xs,ks,zs =  TraceRay(xk0,τ1min,τ1max,D::Function,-8)
+itpx,itpk = get_go_field(sol,phi0)
+plot(xs,ks,show=true)
+#ϕ0 = airyai(τ1min)
